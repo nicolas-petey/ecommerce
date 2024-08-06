70e2b13d369e07ab4359feba6433593c63f7835b
amdqn <dqn.amandine@gmail.com>
2024-08-06 10:21:26 +0200

    feat/user-endpoint : add express route

--- auth-microservice/index.ts
+++ auth-microservice/index.ts
@@ -1,8 +1,19 @@
-const server = Bun.serve({
-    port: 3000,
-    fetch(req) {
-      return new Response("Bun!");
-    },
-  });
-  
-  console.log(`Listening on http://localhost:${server.port} ...`);
+index.ts
+
+import { register } from "./src/controller/authController";
+
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json()); 
+app.post('/api/register', register);
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
